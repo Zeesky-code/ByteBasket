6c41fcb778fd189396f5542279f1a6c90241cd04
Zainab Lawal <zainablaw1012@gmail.com>
2024-05-08 00:08:03 +0300

    Project Setup

---
+++ src/test/java/com/estu/ByteBasket/ByteBasketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estu.ByteBasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteBasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
